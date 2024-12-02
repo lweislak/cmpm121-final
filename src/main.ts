2b0937f55f583eb1111f028a795080c8347db99b
lweislak <150990328+lweislak@users.noreply.github.com>
2024-12-01 22:12:33 -0800

    Implemented player turns
    Added turns that change the sun/water levels. Each turn, the sun level detracts from the water level.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import "./style.css";
 const PADDING = 0;
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
-const DRAW_PLAYER_OFFSET_X = 10;
-const DRAW_PLAYER_OFFSET_Y = 35;
+const DRAW_OFFSET_X = 10;
+const DRAW_OFFSET_Y = 35;
 const BOX_SIZE = 50;
+
+let TIME = 0;
+const TURN = 10;
 
 
 const APP_NAME = "Farming Game";
@@ -26,6 +29,7 @@ interface Player {
   icon: string;
   x: number;
   y: number;
+  //currentSeed: string;
 }
 
 interface Cell {
@@ -34,6 +38,9 @@ interface Cell {
   plantIcon: string | null;
   plantLevel: number | null;
 }
+
+//interface Inventory {}
+
 const player: Player = {
   icon: "👩‍🌾",
   x: 0,
@@ -56,7 +63,7 @@ function drawGrid(){
       ctx.lineTo(CANVAS_WIDTH, 0.5 + y);
   }
 
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = "#291702"; //Dark Brown
   ctx.stroke();
 
   for(let i = 0; i < CANVAS_HEIGHT/BOX_SIZE; i++) {
@@ -69,8 +76,8 @@ function drawGrid(){
 //Displays player on the grid
 function displayPlayer() {
   ctx.font = "24px sans-serif";
-  const x = (player.x * BOX_SIZE) + DRAW_PLAYER_OFFSET_X;
-  const y = (player.y * BOX_SIZE) + DRAW_PLAYER_OFFSET_Y;
+  const x = (player.x * BOX_SIZE) + DRAW_OFFSET_X;
+  const y = (player.y * BOX_SIZE) + DRAW_OFFSET_Y;
   ctx.fillText(player.icon, x, y);
 }
 
@@ -79,20 +86,53 @@ function displayPlayer() {
 function displayPlant(cell: Cell, x: number, y: number) {
   if(cell.plantIcon != null) {
     ctx.font = "24px sans-serif";
-    x = (x * BOX_SIZE) + DRAW_PLAYER_OFFSET_X;
-    y = (y * BOX_SIZE) + DRAW_PLAYER_OFFSET_Y;
+    x = (x * BOX_SIZE) + DRAW_OFFSET_X;
+    y = (y * BOX_SIZE) + DRAW_OFFSET_Y;
     ctx.fillText(cell.plantIcon, x , y);
+
+    //Display plant info
+    ctx.font = "14px sans-serif";
+    ctx.fillStyle = "#dad617"; //Yellow
+    ctx.fillText(cell.sunLevel.toString(), x - 6, y - 20);
+    ctx.fillStyle = "#5faef8"; //Light blue
+    ctx.fillText(cell.waterLevel.toString(), x + 28, y - 20);
+    ctx.fillStyle = "black";
+    ctx.fillText(cell.plantLevel!.toString(), x + 28, y + 14);
   }
 }
 
 //Check which key was pressed
 function checkKeys(key: string) {
-  //Note: 10 is the number of cells in the 2D array
+  //Note: 8 is the number of cells in the 2D array
   if(key == "ArrowUp" && player.y > 0) { player.y--; }
   else if(key == "ArrowDown" && player.y < 7) { player.y++; }
   else if(key == "ArrowLeft" && player.x > 0) { player.x--; }
   else if(key == "ArrowRight" && player.x < 7) { player.x++; }
   else if(key == "KeyE") { sow(); }
+  else { return; }
+  TIME++;
+  checkTurn();
+}
+
+function checkTurn() {
+  if (TIME % TURN == 0) {
+    for(let i = 0; i < CANVAS_HEIGHT/BOX_SIZE; i++) {
+      for(let j = 0; j < CANVAS_WIDTH /BOX_SIZE; j++) {
+        grid[i][j].waterLevel += Math.round(Math.random() * 3);
+        grid[i][j].sunLevel = Math.round(Math.random() * 5);
+        grid[i][j].waterLevel -= grid[i][j].sunLevel; //Sun level decreases water level each turn
+        if(grid[i][j].waterLevel <= 0) { //If water level gets too low, plant dies
+          killPlant(grid[i][j]);
+        }
+      }
+    }
+  }
+}
+
+function killPlant(cell: Cell) { //Could switch to wither instead
+  cell.plantIcon = null;
+  cell.plantLevel = 0;
+  cell.waterLevel = 0;
 }
 
 function sow() {
@@ -102,9 +142,14 @@ function sow() {
     cell.plantIcon = "🌱";
     displayPlant(cell, player.x, player.y);
   }
+  console.log(cell);
 }
 
 /*
+function water() {
+
+}
+
 function reap() {
 
 }
@@ -116,7 +161,7 @@ for(let i = 0; i < CANVAS_HEIGHT/BOX_SIZE; i++) {
   for(let j = 0; j < CANVAS_WIDTH /BOX_SIZE; j++) {
     const cell: Cell = {
       sunLevel: Math.round(Math.random() * 5),
-      waterLevel: Math.round(Math.random() * 5),
+      waterLevel: 0,
       plantIcon: null,
       plantLevel: null,
     }
